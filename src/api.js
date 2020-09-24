6ec28bece2ffbbb8dd0163abff1d5889c5973715
martinalanis <martinalanis.dev@gmail.com>
2020-09-24 17:57:39 -0500

    FIX: Endpoint axios acutalizado Se agergo prefix api para evitar conflicto con rutas de node server

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const api = axios.create({
   //baseURL: "http://10.5.0.7:61"
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:61' : 'http://tiendavirtual.dyndns.org:61'
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:61/api' : 'http://tiendavirtual.dyndns.org:61/api'
 })
 
 api.interceptors.request.use(
